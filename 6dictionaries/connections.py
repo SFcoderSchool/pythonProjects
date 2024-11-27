870d15254f42be5c340267b42471c845a47ae250
Jason <jahuang13@gmail.com>
2024-11-26 17:25:29 -0800

    updated connections

--- 6dictionaries/connections.py
+++ 6dictionaries/connections.py
@@ -24,23 +24,30 @@ connections = {
 words = list(connections.keys())
 random.shuffle(words)
 
+#a function that will print the words in 4 rows of 4 words each
+#they are also numbered based on their index
 def printWords():
     index = 0
     rows = int(len(words)/4)
     for i in range(rows):
         line = ""
         for j in range(4):
-            line += str(index) + ")" + words[index] + " "
+            line += str(index) + ")" + words[index] + " \t"
             index += 1
         print(line)
 
+#a function that will ask the user to select 4 words each
 def select4words():
+    #each word the user select is placed into a list
     selection = []
     for i in range(4):
         num = int(input("Select a word #: "))
         selection.append(words[num])
+    #looks up the word in the dictionary
+    #check if they all have the same topic
     if connections[selection[0]] == connections[selection[1]] == connections[selection[2]] == connections[selection[3]]:
         print("The category was: " + connections[selection[0]])
+        #if they all have the same topic remove the word from the words list
         for i in range(4):
             words.remove(selection[i])
     
@@ -48,6 +55,7 @@ def select4words():
 while True:
     printWords()
     select4words()
+    #if no more words in the words list you got all the words, end the game
     if len(words) == 0:
         print("Congratz you grouped all words!")
         break
