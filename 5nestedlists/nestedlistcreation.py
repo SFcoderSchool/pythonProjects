c1cce568324fd39d5882296485c526a860c1b4a4
Jason <jahuang13@gmail.com>
2024-11-29 18:13:53 -0800

    intro to dictionaries added

--- 5nestedlists/nestedlistcreation.py
+++ 5nestedlists/nestedlistcreation.py
@@ -1,25 +1,23 @@
 
 from pprint import pprint
-#lets start by creating 3 lists called row 1, 2, &3
-row1 = [1,2,3]
-row2 = [7,8,9]
-row3 = [12,13,14]
-#now these 3 lists are independent of each other
-#to group these 3 lists together we put them into another list
-grid = [row1, row2, row3]
-#grid is now a list that holds 3 other lists
-print(grid)
-#to access row1
-print(grid[0])
-#now that we've extablised grid[0] is equivalent to row1
-#lets access the first number of row1
-print(row1[0])
-print(grid[0][0])
-#both are equivalent since grid[0] is row1
-#row1[0] is the same as grid[0][0]
-#Exercise
-#print index 2 of row3 using the grid
-#print index 1 or row2 using the grid
+#create a list of fruits put 4 fruits inside
+#print the first item or second item
+#in the third index add a list in the list
+
+fruits = ["apple","grapes","banana"]
+print(fruits[0])
+
+#add a third item to the list next and have it be a list
+fruits = ["apple","grapes",["oranges", "pears", ""],"banana"]
+
+
+groceries = [
+    ["apple","grapes","banana"],
+    ["beef", "pork"],
+    ["chip","soda"]
+]
+
+#access pork or beef
 
 
 
@@ -47,21 +45,3 @@ pprint(list2d)
 
 
 
-#create a list of fruits put 4 fruits inside
-#print the first item or second item
-#in the third index add a list in the list
-
-fruits = ["apple","grapes","banana"]
-print(fruits[0])
-
-#add a third item to the list next and have it be a list
-fruits = ["apple","grapes",["oranges", "pears", ""],"banana"]
-
-
-groceries = [
-    ["apple","grapes","banana"],
-    ["beef", "pork"],
-    ["chip","soda"]
-]
-
-#access pork or beef
---
+++ 6dictionaries/introtodictionaries.py
@@ -0,0 +1,31 @@
+#a dictionary is like a list of variables and their values
+menu = {
+    "chicken": 6,
+    "bread": 4,
+    "apple": 2
+}
+#in this example "chicken" is holding the value of 6
+#"bread" has the value of 4
+#"apple" has the value of 2
+#we refer to this as the key and their value
+#next we will program how to look up a value in the dictionary
+print(menu["chicken"])
+#the syntax is simlar to accessing data in a list 
+#except we don't use index # but rather we look up what "chicken" is equal to in the dictionary
+#Exercise:
+#1) print out the value of bread in the dictionary
+#2) print out the value of apple in the dictionary
+#3) create a dictionary of technology and their costs
+
+
+#Some useful tools/techniques to remember:
+#all the keys in the dictionary listed out for you
+allKeys = list(menu.keys())
+print(allKeys)
+#iterate through all keys in the list with a for loop
+for i in range(len(allkeys)):
+    print(allKeys[i])
+#print all VALUES in the list by iterating through all keys in the list with a for loop
+for i in range(len(allKeys)):
+    eachKey = allKeys[i]
+    print(menu[eachKey])
