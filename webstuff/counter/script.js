acfb0e58737d8a24ac70177dbe1f5fb8c5252408
Tony Guan <tonyyguan@gmail.com>
2025-07-13 01:38:17 -0700

    more web updates

---
+++ webstuff/counter/script.js
@@ -0,0 +1,26 @@
+let num = 0;
+
+function minus() {
+  num = num - 1;
+  document.getElementById("num").innerHTML = num;
+}
+
+function plus() {
+  num = num + 1;
+  document.getElementById("num").innerHTML = num;
+}
+
+function minus2() {
+  num = num - 2;
+  document.getElementById("num").innerHTML = num;
+}
+
+function plus2() {
+  num = num + 2;
+  document.getElementById("num").innerHTML = num;
+}
+
+function reset() {
+  num = 0;
+  document.getElementById("num").innerHTML = num;
+}
